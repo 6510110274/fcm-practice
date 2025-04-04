9de1a9710c5edb564d3e5956c43c69f9fc1d83d3
Papanya Siriwattanaworakul <papanya2546@gmail.com>
2025-04-04 15:35:46 +0700

    Add Frontend Firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,45 @@
-import { useState } from 'react'
+import { getMessaging, onMessage, getToken } from "firebase/messaging";
+import { toast, ToastContainer } from "react-toastify";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import app from "../config/firebase";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const messaging = getMessaging(app);
+  const vapidKey = import.meta.env.VITE_VAPID_KEY;
+
+  onMessage(messaging, (payload) => {
+    console.log("Message received. ", payload);
+    toast(
+      `Message received. ${payload.notification.title} ${payload?.notification?.body}`
+    );
+  });
+
+  const registerNotify = async () => {
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission !== "granted") {
+        alert("Notification permission denied");
+        return;
+      }
+  
+      const currentToken = await getToken(messaging, { vapidKey });
+  
+      if (!currentToken) {
+        console.warn("No FCM token received");
+        toast.error("Failed to retrieve FCM token");
+        return;
+      }
+  
+      console.log("Token", currentToken);
+      toast.success("Token: " + currentToken);
+    } catch (error) {
+      console.error("Error getting FCM token:", error);
+      toast.error("An error occurred while retrieving the FCM token");
+    }
+  };
 
   return (
     <>
@@ -18,8 +53,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => registerNotify()}>
+          Register Notify
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -28,6 +63,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <ToastContainer />
     </>
   )
 }
